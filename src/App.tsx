e4c85fd758f30856d720afd9a4a2ae3d324b2e85
TheKelvinT <kelvin-tan@flex-solver.com.sg>
2023-06-01 17:11:07 +0800

    scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -15,27 +15,31 @@ import { FloatButton } from 'antd';
 import WhatsappIcon from './assets/whatsapp-logo.svg'
 import useLoadingStore from "./store/loadingStore.js";
 import Admin from "./pages/Admin.js";
+import ScrollToTop from "./components/ScrollToTop.js";
 
 function App() {
   const loading = useLoadingStore((state: any) => state.loading);
-
   return (
     <>
       <BrowserRouter>
+     
       <FloatButton icon={<div className="h-16 w-16 absolute top-1 left-1 opacity-60 hover:opacity-100 transition-all hover:scale-110"><img src={WhatsappIcon} className=""/></div>} tooltip={<div className="">Chat with us now!</div>} type="default" style={{ right: 25 }} href="" className="h-16 w-16 " />
       <Navbar/>
       {/* {loading? "" : <Navbar />} */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/reservations" element={<Reservations />} />
-          <Route path="/careers" element={<Careers />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/admin" element={<Admin/>} />
-        </Routes>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/menu" element={<Menu />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/reservations" element={<Reservations />} />
+            <Route path="/careers" element={<Careers />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/admin" element={<Admin/>} />
+          </Routes>
+        </ScrollToTop> 
         {loading? "" : <Footer/>}
+
       </BrowserRouter>
     </>
   );
