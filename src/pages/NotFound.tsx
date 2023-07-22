33961d932e106f0e89bdc509b108fdf191f8130d
TheKelvinT <tnchnkit@hotmail.com>
2023-07-22 15:19:59 +0800

    page not found

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,16 +1,17 @@
 import LocalButton from '@/components/Button'
-
+import NotFoundImg from '@/assets/not-found.png'
 
 
 const NotFound = () => {
   return (
       <div className="w-full h-screen flex flex-col lg:flex-row items-center justify-center space-y-16 lg:space-y-0 space-x-8 2xl:space-x-0 bg-primary">
     <div className="w-full lg:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center ">
-        <p className="text-7xl md:text-8xl lg:text-9xl font-bold tracking-wider text-gray-300">404</p>
-        <p className="text-4xl md:text-5xl lg:text-6xl font-bold tracking-wider text-gray-300 mt-2">Page Not Found</p>
+  <img src={NotFoundImg} alt="not found" />
+        <p className="text-[40px] mb-12  tracking-wider text-secondary font-marcellus">PAGE NOT FOUND</p>
         
-        <p className="text-lg md:text-xl lg:text-2xl text-gray-500 my-12">Sorry about that! Please visit our homepage to get to where you need to go.</p>
-        <LocalButton title="Return to Homepage" path='/home'/>
+        <p className="text-xl  text-secondary font-marcellus">Sorry, we couldn’t find this page.</p>
+        <p className="text-xl mb-12 text-secondary font-marcellus">But don’t worry, you can find plenty of other things on our homepage.</p>
+        <LocalButton title="Return to Homepage" path='/home' />
     </div>
    
 </div>
